d554adb9ef6827240b721d2991265bda3aefe800
Yair Haimovitch <yairh@wix.com>
2020-07-17 11:04:33 +0300

    remove suite

--- test/gildedRose.test.js
+++ test/gildedRose.test.js
@@ -1,9 +1,7 @@
 const { Shop, Item } = require('../src/gildedRose');
 
-describe('Gilded Rose', function () {
-  it('should foo', function () {
-    const gildedRose = new Shop([new Item('foo', 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('fixme');
-  });
+it('should foo', function () {
+  const gildedRose = new Shop([new Item('foo', 0, 0)]);
+  const items = gildedRose.updateQuality();
+  expect(items[0].name).toBe('fixme');
 });
